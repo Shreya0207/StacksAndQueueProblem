d9869ff412e11f292dcff04cba2c2a8604f20891
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-27 19:16:47 +0530

    [Shreya]Add: Creating the stack using List

--- StackAndQueueProgram/Program.cs
+++ StackAndQueueProgram/Program.cs
@@ -7,6 +7,11 @@ namespace StackAndQueueProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack and Queue Program");
+            LinkedList list = new LinkedList();
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
         }
     }
 }
